e454691d35fb247f2317fe45b5ddfad782457685
cibeles <melaniem@Melanies-MacBook-Air.local>
2025-08-10 18:47:25 -0400

    clean up repo

---
+++ clase1.py
@@ -0,0 +1,43 @@
+# mi_nombre = input('Ingresa tu nombre: ')
+# print('Hola', mi_nombre)
+
+# estudiante = 8
+# suma = 2 + 3
+# print('La suma es:', suma)
+# estudiante = 'claudia'
+# print(type(estudiante))
+# comparacion_1 = 5 > 3
+# comparacion_2 = 5 > 8
+# print(comparacion_1 and comparacion_2) 
+# lado_de_cuadrado = 4.5
+# area_del_cuadrado = lado_de_cuadrado ** 2
+# print('El área del cuadrado es:', area_del_cuadrado)
+# porcion_de_pizza = 6000
+# cantidad_de_porciones_de_pizza = 35
+# costo_total = porcion_de_pizza * cantidad_de_porciones_de_pizza
+# print('El costo total de la pizza es:', costo_total, 'pesos')
+
+# a = int(input('Ingresa un numero A: '))
+# b = int(input('Ingresa un numero B: '))
+# resultado = a * b
+# print('El resultado es:', resultado)
+# edad = 13
+# edad_escrita = str(edad)
+# print('Mi edad es: ' + edad_escrita)
+# numero_en_texto = '109.5'
+# numero = float(numero_en_texto)
+# print(numero)
+# print(numero * 9)
+# for i in range(1, 11):
+#   print(i)
+# lista = [1, 2, 3, 4, 5]
+# print(lista[3])
+# variable = ('Hola',)
+# print(type(variable))
+# lista = [1, 2, 3, 4, 5]
+# lista.append(10)
+# lista.pop(1)
+# print(lista)
+lista = [1, 2, 3, 4, 5]
+lista.pop(3)
+print(lista)
