027462ea095441c61342d59cfd2928324fcb85bf
cibeles <melaniem@Melanies-MacBook-Air.local>
2025-08-10 21:11:59 -0400

    adding comments

--- actividades/actividad1.py
+++ actividades/actividad1.py
@@ -1,21 +1,27 @@
+# initial inventory
 inventario = [
     {"producto": "camisa", "precio": 25900, "stock": "11"},
     {"producto": "pantalón", "precio": 39900, "stock": "23"},
 ]
 
+# adding new products to the inventory
 inventario.append({"producto": "abrigo", "precio": 50000, "stock": "2"}),
 
+# coverting stock to integer
 inventario[0]["stock"] = int(inventario[0]["stock"]);
 inventario[1]["stock"] = int(inventario[1]["stock"]);
 inventario[2]["stock"] = int(inventario[2]["stock"]);
 
+# updating prices
 inventario[0]["precio"] = inventario[0]["precio"] + 10000;
 inventario[1]["precio"] = inventario[1]["precio"] + 10000;
 inventario[2]["precio"] = inventario[2]["precio"] + 10000;
 
+# assigning products to variables
 A = inventario[0];
 B = inventario[1];
 C = inventario[2];
+
 
 print(f"Hay {A['stock']} unidades del producto {A['producto']}. Su precio por unidad es de {A['precio']} pesos.");
 print(f"Hay {B['stock']} unidades del producto {B['producto']}. Su precio por unidad es de {B['precio']} pesos.");
